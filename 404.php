94004b52327a40496a972b9c8f212b770630fb96
Martin Stoppler <stoppe@stoppler.de>
2014-09-27 17:43:14 +0200

    404.php hinzugefügt

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+			<div class="cleft">		
+				<h1>Nicht gefunden :-(</h1>
+
+				<div class="post-content">
+				Die angeforderte Seite konnte leider nicht gefunden werden.
+			
+			</div>
+
+</div>
+<?php get_sidebar('side'); ?>
+
+<?php get_footer(); ?>
