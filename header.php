a81d32d790b62a0aa48395484f2dc7a6a6a1f61d
Martin Stoppler <stoppe@stoppler.de>
2015-07-01 14:31:52 +0200

    add versions to static resources

--- header.php
+++ header.php
@@ -4,11 +4,10 @@
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
     <title><?php wp_title( '»', true, "right" ); ?> <?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
-	<script src="<?php bloginfo('template_url'); ?>/js/jquery-1.11.1.min.js"></script>
-	<script src="<?php bloginfo('template_url'); ?>/js/doubletaptogo.min.js"></script>
+	<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/res/favicon_r1.ico" />
+	<script src="<?php bloginfo('template_url'); ?>/res/jquery-1.11.1.min.js"></script>
+	<script src="<?php bloginfo('template_url'); ?>/res/doubletaptogo_r1.min.js"></script>
 	<script type="text/javascript">
 	
 	$(function () {
@@ -44,7 +43,7 @@
 		<?php get_search_form(); ?>
 		</nav>
 		<header id="mainheader">
-			<a class="homelink" href="<?php bloginfo('url'); ?>"><img alt="<?php bloginfo( "name" ); ?>" src="<?php bloginfo('template_url'); ?>/images/logo.png" id="headerlogo"/><img alt="<?php bloginfo( "name" ); ?>" src="<?php bloginfo('template_url'); ?>/images/logo2.png" id="headerlogo2"/></a>
+			<a class="homelink" href="<?php bloginfo('url'); ?>"><img alt="<?php bloginfo( "name" ); ?>" src="<?php bloginfo('template_url'); ?>res/images/logo_r1.png" id="headerlogo"/><img alt="<?php bloginfo( "name" ); ?>" src="<?php bloginfo('template_url'); ?>res/images/logo2_r1.png" id="headerlogo2"/></a>
 			<div class="sidebar-header">
 				<?php if ( !function_exists('dynamic_sidebar') ||
 			   !dynamic_sidebar('sidebar-header') ) : ?>
